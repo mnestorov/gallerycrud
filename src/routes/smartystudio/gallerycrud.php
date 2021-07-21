63740995d341a1b756a879fc699c21ff64790bfc
Martin Nestorov <mbnestorov@gmail.com>
2021-07-21 18:27:36 +0300

    Minor bug fix on routes namespaces.

--- src/routes/smartystudio/gallerycrud.php
+++ src/routes/smartystudio/gallerycrud.php
@@ -4,19 +4,19 @@
  * Front-end routes
  */
 Route::group(['prefix' => 'gallery'], function () {
-    Route::get('/', ['uses' => '\SmartyStudio\GalleryCrud\App\Http\Controllers\GalleryController@index']);
-    Route::get('/{gallery}/{subs?}', ['as' => 'view-gallery', 'uses' => '\SmartyStudio\GalleryCrud\App\Http\Controllers\GalleryController@show'])
+    Route::get('/', ['uses' => '\SmartyStudio\GalleryCrud\app\Http\Controllers\GalleryController@index']);
+    Route::get('/{gallery}/{subs?}', ['as' => 'view-gallery', 'uses' => '\SmartyStudio\GalleryCrud\app\Http\Controllers\GalleryController@show'])
         ->where(['gallery' => '^((?!admin).)*$', 'subs' => '.*']);
 });
 
 // Glide path
-Route::get('/'.config('smartystudio.gallerycrud.glide_path', 'images').'/{path}', 'SmartyStudio\GalleryCrud\App\Http\Controllers\ImageController@show')->where('path', '.+');
+Route::get('/'.config('smartystudio.gallerycrud.glide_path', 'images').'/{path}', 'SmartyStudio\GalleryCrud\app\Http\Controllers\ImageController@show')->where('path', '.+');
 
 /**
  * Admin routes
  */
 Route::group([
-    'namespace' => 'SmartyStudio\GalleryCrud\App\Http\Controllers\Admin',
+    'namespace' => 'SmartyStudio\GalleryCrud\app\Http\Controllers\Admin',
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', backpack_middleware()],
 ], function () {
